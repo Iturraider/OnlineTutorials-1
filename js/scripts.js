974b47b5ca98ff3c4dfe3d1d39876eab21284ca2
Iturraider <50441706+Iturraider@users.noreply.github.com>
2020-07-19 17:20:09 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var pos = document.documentElement;
-pos.addEventListener("mousemove", e => {
+pos.addEventListener("touchmove", e => {
     pos.style.setProperty('--x', e.clientX + "px");
     pos.style.setProperty('--y', e.clientY + "px");
 })
